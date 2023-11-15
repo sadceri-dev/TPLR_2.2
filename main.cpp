f343c83c870e036cedcc71a2db1d9d797a694a6c
sadceri <70819311+sadceri-dev@users.noreply.github.com>
2023-11-15 16:43:11 +0300

    Вторая часть задания лабораторной работы

--- main.cpp
+++ main.cpp
@@ -1,5 +1,79 @@
-/*
-РЎ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµРј С„Р°Р№Р»РѕРІС‹С… Рё СЃС‚СЂРѕРєРѕРІС‹С… РїРѕС‚РѕРєРѕРІ РЅР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ,
-РєРѕС‚РѕСЂР°СЏ СЃС‡РёС‚С‹РІР°РµС‚ С‚РµРєСЃС‚ РёР· С„Р°Р№Р»Р° Рё РІС‹РІРѕРґРёС‚ РЅР° СЌРєСЂР°РЅ С‚РѕР»СЊРєРѕ С†РёС‚Р°С‚С‹, С‚Рѕ РµСЃС‚СЊ
-РїСЂРµРґР»РѕР¶РµРЅРёСЏ, Р·Р°РєР»СЋС‡РµРЅРЅС‹Рµ РІ РєР°РІС‹С‡РєРё.
-*/
+#include <iostream>
+#include <fstream>
+#include<string>
+
+using namespace std;
+
+bool isDevider(string ch)
+{
+    return (ch == "." || ch == "!" || ch == "?" || ch == "\"");
+}
+
+bool isDeviderExt(string ch)
+{
+    return (ch == "." || ch == "!" || ch == "?" || ch == "\"" || ch == " " || ch == "\t" || ch == "\n");
+}
+
+bool check(string a) // проверка на наличие кавычек в строке
+{
+    if (a.find('<<') != string::npos)
+    {
+        return 0;
+    }
+    else return 1;
+}
+
+void readFile()
+{
+    setlocale(LC_ALL, "Russian");
+
+    ifstream fin; // создаем объект класса ifstream
+    string filename = "text.txt"; // объявляем название файла
+
+    fin.open(filename); // открываем файл
+    if (!fin.is_open()) // если файл не открылся, значит вызываем исключение, что файл не найден
+    {
+        throw "Исключение: файл " + filename + " не найден";
+    }
+
+    string str[100];
+    string c;
+    char ch;
+    int count = 0;
+
+    while (!fin.eof()) // пока файл не пуст
+    {
+        fin.get(ch); // получаем строку (предложение до знака "." или "?" или "!")
+        c = ch;
+
+        if (!isDeviderExt(c)) // пока строка не равна конце предложения (то есть точки и т.д)
+        {
+            while (!fin.eof()) // пока файл не пуст
+            {
+                str[count] += c; // добавляем предложение
+                fin.get(ch);
+                c = ch;
+                if (isDevider(c)) // если это конец предложения
+                {
+                    if (check(str[count])) // проверка на запятую
+                    {
+                        cout << "" << endl;
+                    }
+                    else {
+                        cout << str[count] << endl; // выводим предложения в кавычках
+                    }
+
+                    count++;
+                    break;
+                }
+            }
+        }
+    }
+    fin.close();
+}
+
+
+int main()
+{
+    readFile();
+}
